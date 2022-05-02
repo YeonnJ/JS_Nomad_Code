2a659d1c22af213f916370ff8c561ab78cb03b9d
YeonJae <duswo9595@naver.com>
2022-05-03 00:47:22 +0900

    웹소켓 연결성공

--- zoomClone/src/public/js/app.js
+++ zoomClone/src/public/js/app.js
@@ -1,1 +1,19 @@
-alert("hi");
+const socket = new WebSocket(`ws://${window.location.host}`);
+//현재 내 위치, socket은 서버로의 연결을 의미
+//새로고침 시 작동
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log("New message: ", message.data);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from Server ❌");
+});
+
+setTimeout(() => {
+  socket.send("hello from the browser!");
+}, 10000);
--- zoomClone/src/server.js
+++ zoomClone/src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -6,7 +8,20 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/*", (req, res) => res.redirect("/")); //어딘가로 이동할때 홈으로 다시 보냄
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app); // http서버
+const wss = new WebSocket.Server({ server }); //httpt서버 위에 웹소켓 서버생성
+
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser ✅");
+  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+  socket.on("message", (message) => {
+    console.log(message);
+  });
+  socket.send("hello!!!");
+});
+
+server.listen(3000, handleListen);
